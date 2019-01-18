a90769bc5049e1329e451362f65d213d444c9503
Dongwon Lim <idw111@gmail.com>
2019-01-18 16:51:35 +0900

    updated modules to remove security issues

--- test.js
+++ test.js
@@ -3,15 +3,14 @@ const slackWrite = require('./index');
 const channel = {
     username: 'tester',
     channel: 'C39T210PP',
-    token: 'xoxp-111920032133-111126508304-111160012369-8faa4f3a3beba5d72c16ee698ddd4270'
+    token: 'xoxp-111920032133-111126508304-528441100598-0f7d6a45f52c583a905eb17904f473b9'
 };
 const slack = slackWrite(channel);
 
 describe('slack-write', function() {
     
-
     it('should be capable of posting a plain text', function(done) {
-        slack.write('testing...', (err, result) => {
+        slack.write('testing... ' + Date.now(), (err, result) => {
             expect(result).to.be.an('object');
             expect(result.ok).to.be.ok();
             expect(result.message).to.have.keys(['text']);
@@ -20,7 +19,7 @@ describe('slack-write', function() {
     });
 
     it('should be capable of posting an attachment', function(done) {
-        slack.attach('testing...', {
+        slack.attach('testing... ' + Date.now(), {
             pretext: 'pretext',
             title: 'title',
             text: 'text'
